9944ea975596d7d0056f4be4d02f3612362b4397
Flameofwork <shvetsalexei@gmail.com>
2022-11-25 14:32:09 +0200

    PrivateRoute fix
    PrivateRoute fix

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,10 @@
-import { Navigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/auth/auth-selectors';
 
 function PrivateRoute({ redirectTo = '/' }) {
   const isLoggedIn = useSelector(state => getIsLoggedIn(state));
-  if (isLoggedIn) return <Navigate to={redirectTo} />;
+  if (isLoggedIn) return <Outlet to={redirectTo} />;
 }
 
 export default PrivateRoute;
